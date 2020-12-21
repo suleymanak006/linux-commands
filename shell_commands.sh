f16d36108c8817a7fdcf454793e9f77c10914c51
suleymanak006 <suleymanak006@gmail.com>
2020-12-22 02:27:33 +0300

    shell commands

---
+++ shell_commands.sh
@@ -0,0 +1,15 @@
+pwd
+ls -al
+touch dosya1
+mkdir Dosyalar
+cp dosya1/ Dosyalar
+cd dosya1
+rm dosya1
+echo "Birinci dosya temel shell komutlari" >> dosya2
+cat dosya2
+grep shell dosya2
+echo "Ikinci dosya disk islemleri">>dosya3
+cat dosya2 dosya3 >> dosyatoplam
+cd ..
+rm -r Dosyalar
+sudo sudo
